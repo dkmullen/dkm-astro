0de5de2ea38edbb43468210f9877ea0cec7e1cb9
Dennis Mullen <dkmullen@gmail.com>
2023-02-17 18:46:35 -0500

    Replace axios with fetch

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -43,7 +43,7 @@ document
   .querySelector('#hamburger-button')
   .addEventListener('click', toggleMenu);
 
-async function submitMessage() {
+function submitMessage() {
   let name = document.querySelector('#name').value;
   let email = document.querySelector('#email').value;
   let message = document.querySelector('#message').value;
@@ -51,24 +51,20 @@ async function submitMessage() {
   errorMsg.remove('show');
   errorMsg.add('hide');
   doProgressBar();
-  const res = await axios({
-    method: 'post',
-    url: 'https://sdfh3459a9.execute-api.us-east-2.amazonaws.com/dev',
+  fetch('https://sdfh3459a9.execute-api.us-east-2.amazonaws.com/dev', {
+    method: 'POST',
     headers: {
       'x-api-key': 'i0k0ucR4tW1wmajvQb4XX5GleesDI4Jk2y9l97zd',
     },
-    data: {
+    body: JSON.stringify({
       name,
       email,
       message,
-    },
-  }).catch((err) => {
-    errorMsg.remove('hide');
-    errorMsg.add('show');
-    stopProgressBar();
-  });
-  if (res) {
-    if (res.status === 200) {
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.info('Success:', data);
       stopProgressBar();
       let msg = document.querySelector('#message-received').classList;
       document.querySelector('#contact-form').reset();
@@ -78,12 +74,20 @@ async function submitMessage() {
         msg.remove('show');
         msg.add('hide');
       }, 7000);
-    }
-  }
+    })
+    .catch((error) => {
+      console.error('Error:', error);
+      errorMsg.remove('hide');
+      errorMsg.add('show');
+      stopProgressBar();
+    });
 }
+
 let uName = document.querySelector('#name');
 let email = document.querySelector('#email');
 let message = document.querySelector('#message');
+let submitButton = document.querySelector('#submit-button');
+let contactForm = document.querySelector('#contact-form');
 
 if (uName) {
   uName.addEventListener('input', doValidate);
@@ -92,7 +96,15 @@ if (email) {
   email.addEventListener('input', doValidate);
 }
 if (message) {
-  uName.addEventListener('input', doValidate);
+  message.addEventListener('input', doValidate);
+}
+if (submitButton) {
+  submitButton.addEventListener('mousedown', pressButton);
+  submitButton.addEventListener('mouseup', releaseButton);
+  // submitButton.addEventListener('click', submitMessage);
+}
+if (contactForm) {
+  contactForm.addEventListener('submit', submitMessage);
 }
 
 function doValidate() {
@@ -109,7 +121,7 @@ function doValidate() {
     submitButton.disabled = true;
     submitButton.classList.add('disabled');
   }
-  errMsg = document.querySelector('#email-error-message').classList;
+  let errMsg = document.querySelector('#email-error-message').classList;
   if (email && !emailPattern.test(email)) {
     errMsg.add('show');
     errMsg.remove('hide');
