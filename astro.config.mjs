11e18e0c20d467456ea51d528c29ff82260b15a0
Dennis Mullen <dkmullen@gmail.com>
2023-02-19 11:10:19 -0500

    Add blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dkmullen.github.io',
-  base: '/dkm-astro',
+  base: '',
 });
